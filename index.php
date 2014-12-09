3559eeb3b7f1cfae0558c52307e567f74043b1cf
mshira <mshira@cambium.co.il>
2014-12-09 17:24:01 +0200

    h

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Wikidots</title>
+        <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="main-background"></div>
+        <div class="main-wrapper">
+            <header>
+                <div class="logo"></div>
+                <div class="menu"></div>
+            </header>
+            <div class="main-data-wrapper">
+                <div class="main-data">
+                    <div class="x-button"></div>
+                    <div class="synopsis">
+                        <span>He developed the genera theory.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap int</span>
+                        <span class="learn-more">LEARN MORE</span>
+                    </div>
+                    <div class="highlights-wrapper">
+                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
+                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
+                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
+                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
+                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
+                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 2 pic.png')"></div><div class="high-title">mass-energy</div></div>
+                        <div class="highlights-item" style="margin-right: 0px;"><div class="high-img" style="background-image: url('images/thumb 3 pic.png')"></div><div class="high-title">photolectric</div></div>
+                    </div>
+                </div>
+
+                <div class="footer">
+                    <div class="main-data">
+                        <div class="value-name">Albert Einstein</div>
+                        <div class="desc-name">Theoretical physicist and philosopher</div>
+                    </div>
+                    <div class="hand"></div>
+                
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
