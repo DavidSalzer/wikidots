1a1466df23104e755ac8abce42e07ce237a67fac
mshira <mshira@cambium.co.il>
2014-12-09 23:08:31 +0200

    s

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-$(function(){
-	$(".hand").click(function(){
-		$(".main-data").css("visibility","visible");
-	});
-	$(".x-button").click(function(){
-		$(".main-data").css("visibility","hidden");
-	});
-	
-	$selectedHighlights=null;
-	$(".highlights-item .high-img").click(function(){
-		if ($selectedHighlights!=null && $selectedHighlights[0]==$(this).parent(".highlights-item")[0]){	
-			$selectedHighlights.removeClass("selected");
-			$selectedHighlights=null;
-			$(".main-data").removeClass("point-selected");
-		}
-		else{
-			$selectedHighlights=$(this).parent(".highlights-item");
-			$(".main-data").addClass("point-selected");
-			$selectedHighlights.addClass("selected");
-			$(".main-data.point-selected .synopsis .highlights-text").html($selectedHighlights.attr("data-description"));
-			$(".main-data.point-selected .learn-more").attr("href","?id="+$selectedHighlights.attr("data-id"));
-		}
-	})
-	
+$(function () {
+    $(".hand").click(function () {
+        //$(".main-data").css("visibility","visible");
+        //      $(".main-data").css("opacity","1");
+        $(".main-data").fadeIn();
+    });
+    $(".x-button").click(function () {
+        //$(".main-data").css("visibility","hidden");
+        //       $(".main-data").css("opacity","0");
+        $(".main-data").fadeOut();
+    });
 
-	
-	
+    $selectedHighlights = null;
+    $(".highlights-item .high-img").click(function () {
+        if ($selectedHighlights != null && $selectedHighlights[0] == $(this).parent(".highlights-item")[0]) {
+            $selectedHighlights.removeClass("selected");
+            $selectedHighlights = null;
+            $(".main-data").removeClass("point-selected");
+        }
+        else {
+            $selectedHighlights = $(this).parent(".highlights-item");
+            $(".main-data").addClass("point-selected");
+            $selectedHighlights.addClass("selected");
+            $(".main-data.point-selected .synopsis .highlights-text").html($selectedHighlights.attr("data-description"));
+            $(".main-data.point-selected .learn-more").attr("href", "?id=" + $selectedHighlights.attr("data-id"));
+        }
+    })
+
+
+
+
 
 
 
