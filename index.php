895cc315c2caec99f41b313d56bb3a8f6e1f9e89
mshira <mshira@cambium.co.il>
2014-12-09 22:21:25 +0200

    a

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-$valueID = isset($_GET['id'])?$_GET['id']:"urassic_World";
-
-$dbname = "wikidots";
-$host = "82.80.210.144";  
-$user = "wikidots";
-$pass = "wagoiplrkyjdnvtxemcq"; 
-$db = new PDO('mysql:dbname='.$dbname.';host='.$host, $user, $pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-
-$statement = $db->prepare("select * from value where valueID = :valueID");
-$statement->execute(array(':valueID' => $valueID));
-$row = $statement->fetch(PDO::FETCH_ASSOC); // Use fetchAll() if you want all results, or just iterate over the statement, since it implements Iterator
-if ($row==null)
-	die("404");
-//print_r($row);
+    $valueID = isset($_GET['id'])?$_GET['id']:"urassic_World";
+    
+    $dbname = "wikidots";
+    $host = "82.80.210.144";  
+    $user = "wikidots";
+    $pass = "wagoiplrkyjdnvtxemcq"; 
+    $db = new PDO('mysql:dbname='.$dbname.';host='.$host, $user, $pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    
+    $statement = $db->prepare("select * from value where valueID = :valueID");
+    $statement->execute(array(':valueID' => $valueID));
+    $row = $statement->fetch(PDO::FETCH_ASSOC); // Use fetchAll() if you want all results, or just iterate over the statement, since it implements Iterator
+    if ($row==null)
+        die("404");
+    //print_r($row);
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +23,7 @@ if ($row==null)
         <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <div class="main-background" style="background-image: url('<?php echo $row["imgUrl"]; ?>')";></div>
+        <div class="main-background" style="background-image: url('<?php echo $row["imgUrl"]; ?>')" ;></div>
         <div class="main-wrapper">
             <header>
                 <div class="logo"></div>
@@ -37,11 +37,11 @@ if ($row==null)
                         <span class="learn-more">LEARN MORE</span>
                     </div>
                     <div class="highlights-wrapper">
-						<?php for($i=1;$i<=7;$i++): ?>
-							<?php if ($row["p_name".$i]!=null & $row["p_name".$i]!=""): ?>
-								<div class="highlights-item"><div class="high-img" style="background-image: url('<?php echo $row["p_image_url".$i]; ?>')"></div><div class="high-title"><?php echo $row["p_name".$i]; ?></div></div>
-							<?php endif ?>
-						<?php endfor ?>
+                        <?php for($i=1;$i<=7;$i++): ?>
+                        <?php if ($row["p_name".$i]!=null & $row["p_name".$i]!=""): ?>
+                        <div class="highlights-item"><div class="high-img" style="background-image: url('<?php echo $row["p_image_url".$i]; ?>')"></div><div class="high-title"><?php echo $row["p_name".$i]; ?></div></div>
+                        <?php endif ?>
+                        <?php endfor ?>
                     </div>
                 </div>
 
@@ -53,7 +53,21 @@ if ($row==null)
                     <div class="hand"></div>
                     <div class="timeline">
                         <div class="dot"></div>
+                        <div style="padding-left: 20px;">
+                            <div class="line"></div>
+                            <div class="pink"></div>
+                        </div>
                     </div>
+                </div>
+
+
+            </div>
+
+            <div id="sidebar">
+                <div class="x" onclick="hideSidebar()"></div>
+                <input class="search" type="text" />
+                <div class="options">
+                    <div class="option-item"></div>
                 </div>
             </div>
         </div>
