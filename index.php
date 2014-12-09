ce056fbc9a18e9a87857afbf36e2774e907ac8ef
daniel levanon <ldaniel@cambium.co.il>
2014-12-09 18:40:28 +0200

    get data from db

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-    
+$valueID = isset($_GET['id'])?$_GET['id']:"urassic_World";
+
+$dbname = "wikidots";
+$host = "82.80.210.144";  
+$user = "wikidots";
+$pass = "wagoiplrkyjdnvtxemcq"; 
+$db = new PDO('mysql:dbname='.$dbname.';host='.$host, $user, $pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+$statement = $db->prepare("select * from value where valueID = :valueID");
+$statement->execute(array(':valueID' => $valueID));
+$row = $statement->fetch(PDO::FETCH_ASSOC); // Use fetchAll() if you want all results, or just iterate over the statement, since it implements Iterator
+if ($row==null)
+	die("404");
+//print_r($row);
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +23,7 @@
         <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <div class="main-background"></div>
+        <div class="main-background" style="background-image: url('<?php echo $row["imgUrl"]; ?>')";></div>
         <div class="main-wrapper">
             <header>
                 <div class="logo"></div>
@@ -20,24 +33,22 @@
                 <div class="main-data">
                     <div class="x-button"></div>
                     <div class="synopsis">
-                        <span>He developed the genera theory.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap int</span>
+                        <span><?php echo $row["synopsis"]; ?></span>
                         <span class="learn-more">LEARN MORE</span>
                     </div>
                     <div class="highlights-wrapper">
-                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
-                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
-                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
-                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
-                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 1 pic.png')"></div><div class="high-title">genera relativity</div></div>
-                        <div class="highlights-item"><div class="high-img" style="background-image: url('images/thumb 2 pic.png')"></div><div class="high-title">mass-energy</div></div>
-                        <div class="highlights-item" style="margin-right: 0px;"><div class="high-img" style="background-image: url('images/thumb 3 pic.png')"></div><div class="high-title">photolectric</div></div>
+						<?php for($i=1;$i<=7;$i++): ?>
+							<?php if ($row["p_name".$i]!=null & $row["p_name".$i]!=""): ?>
+								<div class="highlights-item"><div class="high-img" style="background-image: url('<?php echo $row["p_image_url".$i]; ?>')"></div><div class="high-title"><?php echo $row["p_name".$i]; ?></div></div>
+							<?php endif ?>
+						<?php endfor ?>
                     </div>
                 </div>
 
                 <div class="footer">
                     <div class="main-data">
-                        <div class="value-name">Albert Einstein</div>
-                        <div class="desc-name">Theoretical physicist and philosopher</div>
+                        <div class="value-name"><?php echo $row["valueName"]; ?></div>
+                        <div class="desc-name"><?php echo $row["Description"]; ?></div>
                     </div>
                     <div class="hand"></div>
                 
